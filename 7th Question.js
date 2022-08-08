43492d1682bfef7212135860f893ffd163061478
sujoyhalder88 <sujoyhalder88@gmail.com>
2022-08-08 16:12:00 +0530

    modify

--- 7th Question.js
+++ 7th Question.js
@@ -1,22 +1,16 @@
 //Q:7.7. Reverse Integer
-// function reverseInt(x) {
-//     while (x != 0) {
-//         let reversenum = x.toString().split("").reverse().join("");
-//         if (reversenum.endsWith("-")) {
-//             reversenum = "-" + reversenum;
-//             return parseInt(reversenum);
-//         } else {
-//             return parseInt(reversenum);
-//         }
-//     }
-//     if (reversenum > 2147483647) {
-//         return 0;
-//     }
-//     if (reversenum < -2147483647) {
-//         return 0;
-//     }
-// }
-// console.log(reverseInt(-123));
+function reverseInt(x) {
+    while (x != 0) {
+        let reversenum = x.toString().split("").reverse().join("");
+        if (reversenum.endsWith("-")) {
+            reversenum = "-" + reversenum;
+            return parseInt(reversenum);
+        } else {
+            return parseInt(reversenum);
+        }
+    }
+}
+console.log(reverseInt(123));
 
 function revInt(x) {
     let rev = 0;
