bbe0cd7e907295107e27dbc0148c712ad039a6eb
sujoyhalder88 <sujoyhalder88@gmail.com>
2022-08-31 21:10:29 +0530

    add

---
+++ 1st-to-50ty Question/15th Question.js
@@ -0,0 +1,34 @@
+//15. 3Sum
+/*
+Input: nums = [-1,0,1,2,-1,-4]
+Output: [[-1,-1,2],[-1,0,1]]
+*/
+
+var threeSum = function (nums) {
+    // var nums.length = nums.nums.lengthgth;
+    var res = [];
+    var l = 0;
+    var r = 0;
+    nums.sort((a, b) => a - b);
+    console.log(nums);
+    for (var i = 0; i < nums.length; i++) {
+        if (i > 0 && nums[i] === nums[i - 1]) continue;
+        l = i + 1;
+        r = nums.length - 1;
+        while (l < r) {
+            if (nums[i] + nums[l] + nums[r] < 0) {
+                l++;
+            } else if (nums[i] + nums[l] + nums[r] > 0) {
+                r--;
+            } else {
+                res.push([nums[i], nums[l], nums[r]]);
+                while (l < r && nums[l] === nums[l + 1]) l++;
+                while (l < r && nums[r] === nums[r - 1]) r--;
+                l++;
+                r--;
+            }
+        }
+    }
+    return res;
+};
+console.log(threeSum([-1, 0, 1, 2, -1, -4]));
